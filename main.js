6f74597515782de5ac0fbcd40efd0db5fb371521
hosein <hosienlooki0@gmail.com>
2021-12-03 02:36:45 +0330

    add logic of buttons

---
+++ main.js
@@ -0,0 +1,64 @@
+NAME = ""
+NAME_LIST = ''
+console.log('hi')
+
+function load_names_from_storage() {
+    genders = window.localStorage.getItem('genders')
+    if (genders != null) {
+        NAME_LIST = genders
+        console.log(genders)
+
+        document.getElementById('names_list').value = genders
+    }
+}
+
+load_names_from_storage()
+
+function submit() {
+    var requestOptions = {
+        method: 'GET',
+        // redirect: 'follow'
+    };
+    NAME = document.getElementById('name_field').value
+    console.log(NAME)
+    if (NAME.length <= 2) {
+        return
+    }
+    fetch(`https://api.genderize.io/?name=${NAME}`, requestOptions)
+        .then(response => response.json())
+        .then(result => {
+            console.log(result)
+            document.getElementById('predict_gender').innerText = result.gender
+            document.getElementById('predict_value').innerText = result.probability
+        })
+        .catch(error => console.log('error', error));
+}
+
+function clear_radio() {
+    NAME = ''
+    document.getElementById('name_field').value = ''
+
+}
+
+function save() {
+    let radio_value = document.getElementsByName('radio_group')
+    let gender = 'male'
+    radio_value.forEach(element => {
+        if (element.checked === true) {
+            gender = element.value
+        }
+    })
+    if (NAME !== '') {
+        NAME_LIST += NAME +' ' + gender + '\n'
+        window.localStorage.setItem('genders', NAME_LIST)
+    }
+    load_names_from_storage()
+    clear_radio()
+
+}
+
+function remove_local_storage(){
+
+    window.localStorage.setItem('genders','')
+    load_names_from_storage()
+}
